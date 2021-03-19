5e725bfa90d354836dd6211467fec40ae2bbd53c
Elvis de Souza <elvis.desouza99@gmail.com>
2021-03-18 21:57:23 -0300

    sh update

--- download-petroles.sh
+++ download-petroles.sh
@@ -1,2 +1,4 @@
+rm Petroles.conllu
+rm Petroles_2.conllu
 wget http://interrogatorio.ica.ele.puc-rio.br/interrogar-ud/conllu/Petroles.conllu
 wget http://interrogatorio.ica.ele.puc-rio.br/interrogar-ud/conllu/Petroles_2.conllu
