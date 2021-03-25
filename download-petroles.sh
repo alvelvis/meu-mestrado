0b008edd1262bf4e02efac360b23f8d869e875b8
Elvis de Souza <elvis.desouza99@gmail.com>
2021-03-25 12:07:25 -0300

    rename Petroles_v2 to Petroles

--- download-petroles.sh
+++ download-petroles.sh
@@ -2,5 +2,5 @@ rm Petroles.conllu
 rm Petroles_2.conllu
 wget http://interrogatorio.ica.ele.puc-rio.br/interrogar-ud/conllu/Petroles.conllu
 wget http://interrogatorio.ica.ele.puc-rio.br/interrogar-ud/conllu/Petroles_2.conllu
-meld --diff Petroles.conllu Petroles_v2/Petroles.conllu
-meld --diff Petroles_2.conllu Petroles_v2/Petroles_2.conllu
+meld --diff Petroles.conllu Petroles/Petroles.conllu
+meld --diff Petroles_2.conllu Petroles/Petroles_2.conllu
