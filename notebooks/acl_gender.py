9790ede33a19c264fa5af5782ae85b1a26963da7
Elvis de Souza <elvis.desouza99@gmail.com>
2021-06-07 18:22:40 -0300

    notebooks update

--- notebooks/acl_gender.py
+++ notebooks/acl_gender.py
@@ -9,38 +9,27 @@ corpus = estrutura_ud.Corpus(recursivo=True)
 corpus.load(file)
 
 allowed_acl_head = ["PROPN", "NOUN", "SYM", "PRON"]
+feat = "gender"
+deprel = ["acl"]
 
 for sentence in corpus.sentences.values():
     for token in sentence.tokens:
-        if (token.deprel == "acl" and 
-        'gender' in token.__dict__ and 
-        token.__dict__.get("gender", None) != token.head_token.__dict__.get("gender", None)):
-            gender = token.__dict__.get("gender", "none")
-            number = token.__dict__.get("number", "none")
+        if (token.deprel in deprel and 
+        feat in token.__dict__ and 
+        token.__dict__.get(feat, None) != token.head_token.__dict__.get(feat, None)):
+            gender = token.__dict__.get(feat, "none")
             head = copy.copy(token.head_token)
             previous = copy.copy(token.previous_token)
             found = False
-            while head.dephead != "0":
-                head = copy.copy(head.head_token)
-                if (gender == head.__dict__.get("gender", None) and 
-                number == head.__dict__.get("number", None) and 
-                head.upos in allowed_acl_head):
+            while previous.id != "0":
+                previous = copy.copy(previous.previous_token)
+                if (gender == previous.__dict__.get(feat, None) and 
+                previous.upos in allowed_acl_head):
                     if found:
-                        token.misc = token.misc + " ou {} ({})".format(head.word, head.id)
+                        token.misc = token.misc + " ou {} ({})".format(previous.word, previous.id)
                     else:
-                        token.dephead = head.id
+                        token.dephead = previous.id
                         found = True
-            if not found:
-                while previous.id != "0":
-                    previous = copy.copy(previous.previous_token)
-                    if (gender == previous.__dict__.get("gender", None) and 
-                    number == previous.__dict__.get("number", None) and
-                    previous.upos in allowed_acl_head):
-                        if found:
-                            token.misc = token.misc + " ou {} ({})".format(previous.word, previous.id)
-                        else:
-                            token.dephead = previous.id
-                            found = True
 
 file_name = file.rsplit("/", 1)[1]
 corpus.save("regras.conllu")
